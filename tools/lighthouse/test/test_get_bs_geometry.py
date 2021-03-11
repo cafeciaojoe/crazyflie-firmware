a67f99fb1bd9c09507bc494ef5eaf242e415914b
Tim <timothyfist@gmail.com>
2021-03-11 21:13:27 +1100

    second notebook - partially updated code to allow for sideways cases

--- tools/lighthouse/test/test_get_bs_geometry.py
+++ tools/lighthouse/test/test_get_bs_geometry.py
@@ -6,19 +6,23 @@ from cflib.crazyflie.mem import LighthouseBsGeometry
 
 def test_get_bs_geometry_upways():
     upways_data = "sensor_data_1614655937013395.pickle"
-    upways = pickle.load(open(upways_data, "rb"))
-    bs_poses = run_get_bs_geometry_proxy(upways)
+    bs_poses = get_bs_geometry_from_sensor_data(upways_data)
     print(bs_poses)
 
 
 def test_get_bs_geometry_sideways():
     sideways_data = "sensor_data_1614655377286432.pickle"
-    sideways = pickle.load(open(sideways_data, "rb"))
-    bs_poses = run_get_bs_geometry_proxy(sideways)
+    bs_poses = get_bs_geometry_from_sensor_data(sideways_data, sideways=True)
     print(bs_poses)
 
 
-def run_get_bs_geometry_proxy(sensor_vectors_all):
+def get_bs_geometry_from_sensor_data(sensor_data_path, sideways=False):
+    sensor_data = pickle.load(open(sensor_data_path, "rb"))
+    bs_poses = run_get_bs_geometry_proxy(sensor_data, sideways)
+    return bs_poses
+
+
+def run_get_bs_geometry_proxy(sensor_vectors_all, sideways=False):
 
     estimator = LighthouseBsGeoEstimator()
     geometries = {}
@@ -27,7 +31,7 @@ def run_get_bs_geometry_proxy(sensor_vectors_all):
         average_data = sensor_vectors_all[id]
         sensor_data = average_data[1]
         rotation_bs_matrix, position_bs_vector = estimator.estimate_geometry(
-            sensor_data)
+            sensor_data, id, sideways)
         is_valid = estimator.sanity_check_result(position_bs_vector)
         if is_valid:
             geo = LighthouseBsGeometry()
@@ -40,6 +44,6 @@ def run_get_bs_geometry_proxy(sensor_vectors_all):
             # self.print_geo(rotation_bs_matrix, position_bs_vector, is_valid)
 
         else:
-            print("Warning: could not find valid solution for " + id + 1)
+            print(f"Warning: could not find valid solution for {id}")
 
     return geometries
